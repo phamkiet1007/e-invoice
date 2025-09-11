3b09d77bf6dd7c1e348124f870c6f2d4a92eda9a
Kiet Pham <phamkiet173@gmail.com>
2025-09-11 16:57:26 +0700

    refactor/#libs: completed logger.middleware

---
+++ libs/constants/src/lib/common.constant.ts
@@ -0,0 +1,6 @@
+export const PORT = 3000;
+
+export enum MetedataKeys {
+  PROCESS_ID = 'processId',
+  START_TIME = 'startTime',
+}
--- libs/middlewares/src/lib/logger.middleware.ts
+++ libs/middlewares/src/lib/logger.middleware.ts
@@ -1,10 +1,39 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { getProcessId } from '@common/utils/string.util';
+import { MetedataKeys } from '@common/constants/common.constant';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(`Request..`);
+    const startTime = Date.now();
+    const { method, originalUrl, body } = req;
+
+    const processId = getProcessId();
+
+    const now = Date.now();
+
+    (req as any)[MetedataKeys.PROCESS_ID] = processId; //gán processId vào req để các middleware khác có thể sử dụng
+    (req as any)[MetedataKeys.START_TIME] = startTime; //gán startTime vào req để các middleware khác có thể sử dụng
+
+    Logger.log(
+      `HTTP >> Start process '${processId}' >> path: '${originalUrl}' >> method: '${method}' >> at: '${now}' >> input: ${JSON.stringify(
+        body,
+      )}`,
+    );
+
+    const originalUrlSend = res.send.bind(res);
+
+    res.send = (body: any) => {
+      const duration = Date.now() - startTime;
+
+      Logger.log(
+        `HTTP >> End process '${processId}' >> path: '${originalUrl}' >> method: '${method}' >> at: '${now}' >> duration: ${duration}ms `,
+      );
+
+      return originalUrlSend(body);
+    };
+
     next();
   }
 }
