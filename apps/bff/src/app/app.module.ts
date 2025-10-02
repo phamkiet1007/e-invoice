802d8b1180d6869cdad1bc75ea74b3cad7bfb12a
Kiet Pham <phamkiet173@gmail.com>
2025-10-02 18:26:45 +0700

    feat/#: init tcp

--- apps/bff/src/app/app.module.ts
+++ apps/bff/src/app/app.module.ts
@@ -6,9 +6,19 @@ import { CONFIGURATION, TConfiguration } from '../configuration';
 import { LoggerMiddleware } from '@common/middlewares/logger.middleware';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ExceptionInterceptor } from '@common/interceptors/exception.interceptor';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] }),
+    ClientsModule.register([
+      {
+        name: 'TCP_INVOICE_SERVICE',
+        transport: Transport.TCP,
+        options: { host: 'localhost', port: 3301 },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_INTERCEPTOR, useClass: ExceptionInterceptor }],
 })
--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -7,7 +7,6 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { options } from 'axios';
 
 async function bootstrap() {
   try {
